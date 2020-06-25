eb4923690bd85b8ce29d9411e7b717d7cbf77371
jeanne.gardebois <jeanne.gardebois@gmail.com>
2020-06-25 17:50:51 +0200

    [JAVA][INIT] project initialization

---
+++ final.legend/webapp/src/main/java/com/mti/FinalLegendApplication.java
@@ -0,0 +1,14 @@
+package com.mti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalLegendApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(FinalLegendApplication.class, args);
+    }
+
+}
